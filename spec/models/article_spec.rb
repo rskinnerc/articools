249866b9d688f7ec20e5916ad8c06a6949c18420
Ronald Skinner <rjskinnerc@unal.edu.co>
2022-12-02 10:41:40 -0500

    Write basic article model tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    article = Article.new(title: 'Title', body: 'Text')
+    expect(Article.new).to be_valid
+  end
+
+  it 'is not valid without a title' do
+    article = Article.new(title: nil)
+    expect(article).to_not be_valid
+  end
+
+  it 'is not valid without a body' do
+    article = Article.new(body: nil)
+    expect(article).to_not be_valid
+  end
+
+  it 'should have a search method' do
+    expect(Article).to respond_to(:search)
+  end
 end
