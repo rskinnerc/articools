2725eec62d935449b17ad277660168578b9ca129
Ronald Skinner <rjskinnerc@unal.edu.co>
2022-12-03 11:55:17 -0500

    Add search_session_id hdden field

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,18 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
 import { Subject } from "rxjs"
 import { debounceTime } from "rxjs"
+import { v4 as uuid } from "uuid"
 
 export default class extends Controller {
-  static targets  = ["query"]
+  static targets  = ["query", "searchSessionId"]
 
   newInput = new Subject()
 
+  initialize() {
+    this.generateSearchSessionId()
+  }
   connect() {
     this.setupInputEvent()
   }
 
   debounceInput() {
-    this.newInput.next(this.queryTarget.value)
+    this.queryTarget.value.length > 0 ? this.newInput.next(this.queryTarget.value) : this.generateSearchSessionId()
   }
 
   setupInputEvent() {
@@ -21,5 +25,9 @@ export default class extends Controller {
     ).subscribe(() => {
       this.element.requestSubmit()
     })
+  }
+
+  generateSearchSessionId() {
+    this.searchSessionIdTarget.value = uuid()
   }
 }
