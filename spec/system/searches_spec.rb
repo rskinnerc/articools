52e17402e19e2b837a145782f696230bcf14c078
Ronald Skinner <rjskinnerc@unal.edu.co>
2022-12-04 08:37:39 -0500

    Refactor system tests to use fixtures

--- spec/system/searches_spec.rb
+++ spec/system/searches_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Searches', type: :system do
+  fixtures :articles
   before do
     driven_by(:selenium_chrome_headless)
   end
@@ -12,9 +13,8 @@ RSpec.describe 'Searches', type: :system do
     end
 
     it 'renders the latest 3 articles' do
-      4.times { Article.create(title: 'Title', body: 'Text') }
       visit root_path
-      expect(page).to have_selector('.article', count: 3)
+      expect(page).to have_selector('.article', count: 2)
     end
 
     it 'renders the search form' do
@@ -23,26 +23,20 @@ RSpec.describe 'Searches', type: :system do
     end
 
     it 'renders the search results when the user types' do
-      2.times { Article.create(title: 'Title', body: 'Text') }
-      2.times { Article.create(title: 'Test', body: 'Text') }
-
       visit root_path
       sleep 0.3
-      fill_in 'query', with: 'Title'
-      expect(page).to have_selector('.result', count: 2)
+      fill_in 'query', with: 'Second'
+      expect(page).to have_selector('.result', count: 1)
     end
 
     it 'renders the search results when the user types two different strings' do
-      2.times { Article.create(title: 'Title', body: 'Text') }
-      3.times { Article.create(title: 'Test', body: 'Text') }
-
       visit root_path
       sleep 0.3
-      fill_in 'query', with: 'Title'
-      expect(page).to have_selector('.result', count: 2)
+      fill_in 'query', with: 'First'
+      expect(page).to have_selector('.result', count: 1)
       sleep 0.3
-      fill_in 'query', with: 'Test'
-      expect(page).to have_selector('.result', count: 3)
+      fill_in 'query', with: 'Second'
+      expect(page).to have_selector('.result', count: 1)
     end
   end
 end
